c48e6c42b32658d92e3273136b3338689e6b3304
Marisa Richmond <marisa.richmond94@gmail.com>
2019-11-09 16:02:53 -0600

    Initial Commit - Set up webpack and webpack dev server - Set up Babel to transpile es6 code into es5 code that the browser can understand

---
+++ webapp/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App(props) {
+	return (
+		<>Color Wheel</>
+	);
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
