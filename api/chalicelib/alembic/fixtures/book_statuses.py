50ebb70b48f532a67341f7d645aad525d562aed8
Marisa Richmond <marisa.richmond94@gmail.com>
2021-06-12 17:18:08 -0500

    Commit

---
+++ api/chalicelib/alembic/fixtures/book_statuses.py
@@ -0,0 +1,38 @@
+book_statuses_fixture = [
+        {
+            'id': 'bef88212-a634-495b-984f-1db61bfd7235',
+            'name': 'pre-writing',
+            'display_name': 'Pre-Writing',
+            'order_index': 0,
+        },
+        {
+            'id': '606246d3-b0d8-4621-b9ea-319035996a90',
+            'name': 'writing',
+            'display_name': 'Writing',
+            'order_index': 1,
+        },
+        {
+            'id': 'c8262d58-2ca3-427f-9e60-af75c2505e20',
+            'name': 'editting',
+            'display_name': 'Editting',
+            'order_index': 2,
+        },
+        {
+            'id': '66a1e4f2-d305-47ef-aed0-debb102ca6d2',
+            'name': 'reviewing',
+            'display_name': 'Reviewing',
+            'order_index': 3,
+        },
+        {
+            'id': 'a3feedcb-8bc4-4f27-9cd2-acbef2d6a624',
+            'name': 'revising',
+            'display_name': 'Revising',
+            'order_index': 4,
+        },
+        {
+            'id': 'ba8b385e-587f-4109-ab4b-00c5310a66df',
+            'name': 'publishing',
+            'display_name': 'Publishing',
+            'order_index': 5,
+        },
+    ]
--- api/chalicelib/alembic/versions/001_initial_database_creation.py
+++ api/chalicelib/alembic/versions/001_initial_database_creation.py
@@ -10,6 +10,8 @@ import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.orm import sessionmaker
 
+from alembic.fixtures import book_statuses_fixture
+
 # revision identifiers, used by Alembic.
 revision = '000000000001'
 down_revision = None
@@ -24,6 +26,7 @@ def upgrade():
     session = Session(bind=bind)
     session.execute('create extension if not exists "uuid-ossp";')
 
+    ### DIM BOOK STATUSES ###
     book_statuses_table = op.create_table(
         'dim_book_statuses',
         sa.Column(
@@ -37,6 +40,9 @@ def upgrade():
         sa.Column('order_index', sa.Integer(), nullable=False),
         sa.PrimaryKeyConstraint('id')
     )
+    op.bulk_insert(book_statuses_table, book_statuses_fixture)
+
+    ### DIM USERS ###
     op.create_table(
         'dim_users',
         sa.Column(
@@ -51,6 +57,8 @@ def upgrade():
         sa.PrimaryKeyConstraint('id'),
         sa.UniqueConstraint('email')
     )
+
+    ### FCT SESSIONS ###
     op.create_table(
         'fct_sessions',
         sa.Column(
@@ -71,46 +79,6 @@ def upgrade():
         sa.PrimaryKeyConstraint('id'),
         sa.UniqueConstraint('token')
     )
-
-    book_statuses_fixture = [
-        {
-            'id': 'bef88212-a634-495b-984f-1db61bfd7235',
-            'name': 'pre-writing',
-            'display_name': 'Pre-Writing',
-            'order_index': 0,
-        },
-        {
-            'id': '606246d3-b0d8-4621-b9ea-319035996a90',
-            'name': 'writing',
-            'display_name': 'Writing',
-            'order_index': 1,
-        },
-        {
-            'id': 'c8262d58-2ca3-427f-9e60-af75c2505e20',
-            'name': 'editting',
-            'display_name': 'Editting',
-            'order_index': 2,
-        },
-        {
-            'id': '66a1e4f2-d305-47ef-aed0-debb102ca6d2',
-            'name': 'reviewing',
-            'display_name': 'Reviewing',
-            'order_index': 3,
-        },
-        {
-            'id': 'a3feedcb-8bc4-4f27-9cd2-acbef2d6a624',
-            'name': 'revising',
-            'display_name': 'Revising',
-            'order_index': 4,
-        },
-        {
-            'id': 'ba8b385e-587f-4109-ab4b-00c5310a66df',
-            'name': 'publishing',
-            'display_name': 'Publishing',
-            'order_index': 5,
-        },
-    ]
-    op.bulk_insert(book_statuses_table, book_statuses_fixture)
     # ### end Alembic commands ###
 
 
